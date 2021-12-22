3ffc253e9393afaa949229de829314eb1e85d977
JR-LAPTOP01\JR <a20629@alunos.ipca.pt>
2021-12-22 21:10:31 +0000

    Aditional angular setup with ng add angular material and creating the auth/login auth/signup transactions and transactions/new-transaction modules

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { TransactionsComponent } from './transactions/transactions.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: WelcomeComponent },
+  { path: 'signup', component: SignupComponent},
+  { path: 'login', component: LoginComponent},
+  { path: 'transactions', component: TransactionsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
