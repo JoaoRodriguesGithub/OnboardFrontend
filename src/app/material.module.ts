87107c095e9a6b34de90bc086647c3def8f54fe1
Jorge Rodrigues <a20632@alunos.ipca.pt>
2021-12-26 18:48:53 +0000

    Conding past transactions table with position,date,category and amount. Fixing next button on new transactions.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -6,11 +6,12 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatListModule } from '@angular/material/list';
-import {MatSelectModule} from '@angular/material/select';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatSelectModule } from '@angular/material/select';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
-import {MatStepperModule} from '@angular/material/stepper';
+import { MatStepperModule } from '@angular/material/stepper';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   imports: [
@@ -26,6 +27,7 @@ import {MatStepperModule} from '@angular/material/stepper';
     MatDatepickerModule,
     MatNativeDateModule,
     MatStepperModule,
+    MatTableModule,
   ],
   exports: [
     MatButtonModule,
@@ -40,6 +42,7 @@ import {MatStepperModule} from '@angular/material/stepper';
     MatDatepickerModule,
     MatNativeDateModule,
     MatStepperModule,
+    MatTableModule,
   ],
 })
-export class MaterialModule {}
+export class MaterialModule { }
